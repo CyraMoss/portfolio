6443c2bfec6d4653f62a7b936e3621388d74877a
Cyra Moss <114317802+CyraMoss@users.noreply.github.com>
2023-09-04 20:53:31 +1200

    aded dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,10 @@ export default function Home() {
         <meta name="description" content="Cyra Moss portfolio. Software Developer from Auckland, NZ" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
+      <main className="m-4 bg-slate-200 h-screen">
        <div>
         <DarkModeToggle/>
-        <div>
+        <div >
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Cyra Moss </h1>
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
       Software Developer from Auckland, New Zealand
